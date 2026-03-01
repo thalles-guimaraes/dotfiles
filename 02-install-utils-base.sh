18c422657424fc51e9cb298e1b1e734a6b88609a
Thalles <thalles.guimaraes456@gmail.com>
2026-03-01 19:24:49 -0300

    Update

--- 02-install-utils-base.sh
+++ 02-install-utils-base.sh
@@ -4,7 +4,7 @@
 set -e
 
 echo "========================================="
-echo "  SCRIPT DE PÓS-INSTALAÇÃO - HYPRLAND    "
+echo "  SCRIPT DE PÓS-INSTALAÇÃO - UTILS    "
 echo "========================================="
 
 # ---------------------------------------------------
@@ -93,12 +93,6 @@ if dmesg | grep -iq bluetooth || lsusb | grep -iq bluetooth || lspci | grep -iq 
 else
     echo "-> Nenhum Bluetooth físico detectado. Pulando..."
 fi
-
-# ---------------------------------------------------
-# 6. INSTALANDO HYPRLAND (E ECOSSISTEMA)
-# ---------------------------------------------------
-echo "[6/6] Instalando Hyprland..."
-sudo pacman -S --needed --noconfirm hyprland kitty firefox waybar rofi-wayland dunst xdg-desktop-portal-hyprland polkit-kde-agent
 
 echo "========================================="
 echo " INSTALAÇÃO CONCLUÍDA COM SUCESSO!       "
---
+++ 03-install-hyprland-base.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Habilita parada por erro, mas desativaremos temporariamente quando necessário
+set -e
+
+echo "========================================="
+echo "  SCRIPT DE PÓS-INSTALAÇÃO - HYPRLAND    "
+echo "========================================="
+
+# ---------------------------------------------------
+# 6. INSTALANDO HYPRLAND (E ECOSSISTEMA)
+# ---------------------------------------------------
+echo "[6/6] Instalando Hyprland..."
+sudo pacman -S --needed --noconfirm hyprland kitty firefox waybar rofi-wayland dunst xdg-desktop-portal-hyprland polkit-kde-agent
+
